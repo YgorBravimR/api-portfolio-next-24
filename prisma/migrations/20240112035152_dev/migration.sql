43cffdd518d1b7e29780feb470e039a8596d78d8
Ygor Bravim INSIDE <ygor@insidethebox.com.br>
2024-01-12 02:18:09 -0300

    feat: create prisma setup, initial migration

---
+++ prisma/migrations/20240112035152_dev/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "messages" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "created_at" TEXT NOT NULL
+);
